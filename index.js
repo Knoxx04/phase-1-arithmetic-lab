f88a0f610abfe8f6678cc142141197484ba683a9
Knoxx04 <onyiknoxx3@gmail.com>
2022-11-29 09:34:05 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,20 @@
 
-// Write your code here
+const num1= 31;
+const num2= 2;
+const multiply=num1*num2
+console.log(num1);
+console.log(num2);
+console.log(multiply);
+
+const random= Math.floor(Math.random() * 10) + 1;
+console.log(random)
+
+const num3=64;
+const num4=16;
+const mod=num3/num4;
+console.log(num3);
+console.log(num4);
+console.log(mod);
+
+const max = 20;
+console.log(max);
